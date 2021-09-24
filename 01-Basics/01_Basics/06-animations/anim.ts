6dbe789f19caafd2ffc172ad45cefce2929bf116
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2021-09-24 15:15:07 -0700

    learned how to add attributes, its all over now if I can transfer attribs from houdini.

--- 01-Basics/01_Basics/06-animations/anim.ts
+++ 01-Basics/01_Basics/06-animations/anim.ts
@@ -1,22 +1,31 @@
-import * as THREE from "three";
-import "../06-animations/style.css";
-const canvas = document.querySelector("canvas.webgl");
+import * as THREE from 'three'
+import { BufferAttribute } from 'three'
+import '../06-animations/style.css'
+const canvas = document.querySelector('canvas.webgl')
 
 const sizes = {
     height: window.innerHeight,
     width: window.innerWidth,
-};
+}
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
     // update sizes
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
     // we put renderer.setSize here and with our renderer
-    renderer.setSize(sizes.width, sizes.height);
-});
+    renderer.setSize(sizes.width, sizes.height)
+})
+
+window.addEventListener('dblclick', () => {
+    if (!document.fullscreenElement) {
+        canvas.requestFullscreen()
+    } else {
+        document.exitFullscreen()
+    }
+})
 
 //CURSOR
 //	|
@@ -24,26 +33,56 @@ window.addEventListener("resize", () => {
 const cursorLocation = {
     x: 0,
     y: 0,
-};
+}
 
-window.addEventListener("mousemove", (event: any) => {
+window.addEventListener('mousemove', (event: any) => {
     // clientX and clientY
     // usually we want a range from -1 to 1
 
-    cursorLocation.x = event.clientX / sizes.width - 0.5;
-    cursorLocation.y = -(event.clientY / sizes.height - 0.5);
-});
+    cursorLocation.x = event.clientX / sizes.width - 0.5
+    cursorLocation.y = -(event.clientY / sizes.height - 0.5)
+})
 
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 //OBJ
 //	|
 //	v
-const geo = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-const mats = new THREE.MeshNormalMaterial();
-const mesh = new THREE.Mesh(geo, mats);
+const geo = new THREE.BoxGeometry(0.5, 0.5, 0.5)
 
-scene.add(mesh);
+// float32 array info
+const positionsArray = new Float32Array([
+    0, 0, 0, 0, 1, 0, 1, 0, 0,
+])
+const posAttrib = new THREE.BufferAttribute(
+    positionsArray,
+    3,
+)
+
+const geometry = new THREE.BufferGeometry()
+
+let count = 50
+const posArr = new Float32Array(count * 3 * 3)
+for (let i = 0; i < count * 3 * 3; i++) {
+    posArr[i] = Math.random() - 0.5
+}
+
+const positionAttribute = new THREE.BufferAttribute(
+    posArr,
+    3,
+)
+
+geometry.setAttribute('position', positionAttribute)
+geometry.computeVertexNormals()
+// takes string attribute: position is one of the attribs
+// geometry.setAttribute('position', posAttrib)
+// why 9? fill array with values, its xyz coords!
+
+const mats = new THREE.MeshBasicMaterial({
+    wireframe: true,
+})
+const mesh = new THREE.Mesh(geometry, mats)
+scene.add(mesh)
 
 //CAMERA/RENDER
 //	|
@@ -53,15 +92,15 @@ const camera = new THREE.PerspectiveCamera(
     45,
     sizes.width / sizes.height,
     0.1,
-    100
-);
+    100,
+)
 // update camera
 
-camera.position.z = -5;
-scene.add(camera);
+camera.position.z = -5
+scene.add(camera)
 
 // ORTHO CAMERA
-const aspectRatio = sizes.width / sizes.height;
+const aspectRatio = sizes.width / sizes.height
 
 const orthoCamera = new THREE.OrthographicCamera(
     -1 * aspectRatio,
@@ -69,8 +108,8 @@ const orthoCamera = new THREE.OrthographicCamera(
     1,
     -1,
     0.1,
-    100
-);
+    100,
+)
 
 // creating controls
 // const controls = new OrbitControls(camera, canvas);
@@ -78,11 +117,11 @@ const orthoCamera = new THREE.OrthographicCamera(
 const renderer = new THREE.WebGLRenderer({
     canvas,
     antialias: true,
-});
+})
 
-renderer.setSize(sizes.width, sizes.height);
+renderer.setSize(sizes.width, sizes.height)
 // use this for performance balancing.
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 //GSAP ANIMATION
 //	|
@@ -93,15 +132,15 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // })
 
 // ANIMATIONS
-(function tick() {
+;(function tick() {
     // update objects
-    let current = Date.now();
+    let current = Date.now()
 
-    camera.position.x = Math.sin(cursorLocation.x * Math.PI);
-    camera.position.y = Math.cos(cursorLocation.y * Math.PI);
-    camera.lookAt(mesh.position);
+    camera.position.x = Math.sin(cursorLocation.x * Math.PI)
+    camera.position.y = Math.cos(cursorLocation.y * Math.PI)
+    camera.lookAt(mesh.position)
 
     //render
-    renderer.render(scene, camera);
-    window.requestAnimationFrame(tick);
-})();
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+})()
